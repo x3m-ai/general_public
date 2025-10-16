f40fe612f2ef149028d8836094396178810668e6
X3M.AI <165936278+x3m-ai@users.noreply.github.com>
2025-10-16 20:15:31 +0100

    Update installation message for Morgana

--- install-morgana-vm.sh
+++ install-morgana-vm.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 set -e
 echo "========================================================"
-echo "  MORGANA INSTALLATION"
+echo "  MORGANA INSTALLATION2"
 echo "========================================================"
 
 if [ "$EUID" -ne 0 ]; then 
